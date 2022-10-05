64e62c5f54e397a00cd081d6ca4ccba6d942a3d9
waterm80 <waterm80@gmail.com>
2022-10-06 03:17:40 +0800

    with_ann

--- cam.py
+++ cam.py
@@ -4,6 +4,7 @@ from model import CNN_Model
 from torchvision import transforms
 from PIL import Image
 import numpy as np
+import openpifpaf
 
 vid_cam = cv2.VideoCapture(0)
 model = CNN_Model()
@@ -12,17 +13,25 @@ model.eval()
 
 while(vid_cam.isOpened()):
     ret, frame = vid_cam.read()
-
     img = Image.fromarray(frame)
+
+    predictor = openpifpaf.Predictor(checkpoint='shufflenetv2k16')
+    predictions, gt_anns, image_meta = predictor.pil_image(img)
+
+    annotation_painter = openpifpaf.show.AnnotationPainter()
+    with openpifpaf.show.image_canvas(frame, fig_file = 'out.jpg') as ax:
+        annotation_painter.annotations(ax, predictions)
+
+    im = Image.open('out.jpg').convert('RGB')
+
     convert_tensor = transforms.Compose([transforms.Resize((224,224)), transforms.ToTensor()])
-    img_tensor = convert_tensor(img)
+    img_tensor = convert_tensor(im)
     unsqueeze_tensor = img_tensor.unsqueeze(0)
     with torch.no_grad(): 
         output = model(unsqueeze_tensor).numpy()
 
     font = cv2.FONT_HERSHEY_DUPLEX
-    color = (0, 255, 255)
-    fontsize = 255
+    color = (0, 0, 255)
     position = (10, 40)
     text1 = 'Clear'
     text2 = 'Crossing'
